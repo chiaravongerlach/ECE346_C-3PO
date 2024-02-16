1a3964cc11e6f1881ef2173885b26dfc89f12907
Chiara von Gerlach <chiaravongerlach@dynamic-oit-vapornet100-10-9-111-0.princeton.edu>
2024-02-15 19:11:00 -0500

    debugging direction

--- ROS_Core/src/Labs/Lab0/scripts/controller/pure_pursuit.py
+++ ROS_Core/src/Labs/Lab0/scripts/controller/pure_pursuit.py
@@ -167,7 +167,7 @@ class PurePursuitController():
         ########################### END OF TODO 4 #################################
             
         msg = ServoMsg()
-        msg.header.stamp = rospy.get_time()
+        msg.header.stamp = rospy.get_rostime()
         msg.throttle = accel
         msg.steer = steer
         self.control_pub.publish(msg)
@@ -208,11 +208,11 @@ class PurePursuitController():
                     elif np.abs(alpha) > np.pi /2:
                         reference_velocity = self.max_vel
                         if alpha > 0:
-                            steer = - self.max_steer
+                            steer =  -self.max_steer
                         else:
                             steer = self.max_steer
                     else:
-                        steer =np.arctan(2*self.wheel_base*np.sin(alpha) / min(self.ld_max, dis2goal))
+                        steer =-np.arctan(2*self.wheel_base*np.sin(alpha) / min(self.ld_max, dis2goal))
                         reference_velocity = min(self.max_vel, dis2goal-self.stop_distance)
 
                     steer = np.clip(steer, -self.max_steer, -self.max_steer)
